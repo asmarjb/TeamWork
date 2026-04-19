f6f55ef71efab91163ccf59ddb89e838a7e67367
Khumar A. Osmanova <khumarao@code.local>
2026-04-19 14:49:25 +0400

    dessd

---
+++ VehicleBicycle/Models/Bycycle.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VehicleBicycle.Models
+{
+    public class Bycycle : Vehicle 
+    {
+        public int WheelNum { get; set; }
+        public void ShowFullInfo() 
+        {
+            Console.WriteLine($"{Brand}, {Model}, {Mile}, {WheelNum}");
+        }
+    }
+}
