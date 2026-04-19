1aa9240195b21df914eb34eedf8730d5c3080661
Khumar A. Osmanova <khumarao@code.local>
2026-04-19 14:27:18 +0400

    de

---
+++ VehicleBicycle/Models/Vehicle.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VehicleBicycle.Models
+{
+    public class Vehicle
+    {
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public double Mile { get; set; }
+
+    }
+}
