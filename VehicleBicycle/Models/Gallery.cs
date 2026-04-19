49a95fad1d1b9f3a02f4a2e5bcdad0c8f8f13d3a
asmarjb <asmarjb@code.edu.az>
2026-04-19 15:07:09 +0400

    1234

---
+++ VehicleBicycle/Models/Gallery.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VehicleBicycle.Models
+{
+    internal class Gallery
+    {
+        List <Car> cars = new List<Car>();
+        public void AddCar(Car car)
+        {
+            cars.Add(car);
+        }
+    }
+}
